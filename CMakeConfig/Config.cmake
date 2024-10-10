42819340a8d53f97864cab6c56e95b6a0f17a9eb
Dmytro <dvshalimov@gmail.com>
2024-10-10 22:34:02 +0200

    initial configuration

---
+++ CMakeConfig/Config.cmake
@@ -0,0 +1,22 @@
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+if(NOT isConfigDone)
+
+    set(isConfigDone TRUE)
+
+    get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
+    if(isMultiConfig)
+        set(CMAKE_CONFIGURATION_TYPES "Debug;MinSizeRel;Editor" CACHE STRING "" FORCE) 
+    else()
+        message(FATAL_ERROR "Only MULTI_CONFIG is supported!")
+    endif()
+    
+    set(CMAKE_CXX_FLAGS_EDITOR "${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_C_FLAGS_EDITOR "${CMAKE_C_FLAGS_DEBUG}")
+    set(CMAKE_EXE_LINKER_FLAGS_EDITOR "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+    set(CMAKE_SHARED_LINKER_FLAGS_EDITOR "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+
+endif()
